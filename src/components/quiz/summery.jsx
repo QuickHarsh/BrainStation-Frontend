db33a0fb6e56fc23796d68def9231505d6191e0e
danuja01 <it21023378@my.sliit.lk>
2024-09-08 20:06:01 +0530

    Feat: Add QuizSummery component for displaying quiz summary

---
+++ src/components/quiz/summery.jsx
@@ -0,0 +1,12 @@
+const QuizSummery = ({ onClose }) => {
+  return (
+    <div>
+      hi
+      <button className="absolute top-4 right-4 text-xl" onClick={onClose}>
+        &times;
+      </button>
+    </div>
+  );
+};
+
+export default QuizSummery;
