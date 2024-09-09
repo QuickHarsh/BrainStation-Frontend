f8d7076e09ed0d09e77f7c7d0061bf12daf64313
danuja01 <it21023378@my.sliit.lk>
2024-09-10 00:57:56 +0530

    Feat: Intergrate module APIs

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,28 +1,58 @@
+import Scrollbars from "react-custom-scrollbars-2";
+import { useDispatch, useSelector } from "react-redux";
+import { Loader } from "@/components";
 import ModuleCard from "@/components/cards/module-card";
-
-// Sample data, ensure you have correct data structure
-const modules = [
-  { moduleId: 1, title: "Foundations of Computing: Data Structures, Algorithms, and Operating Systems", progress: 50 },
-  { moduleId: 2, title: "Module 2", progress: 30 },
-  { moduleId: 3, title: "Module 3", progress: 70 }
-];
+import useFetchData from "@/hooks/fetch-data";
+import { getAllModules } from "@/service/module";
+import { setModules } from "@/store/moduleSlice";
 
 const Main = () => {
+  const dispatch = useDispatch();
+  const modules = useSelector((state) => state.modules.modules);
+
+  const modulesData = useFetchData(getAllModules);
+  console.log(modulesData);
+
+  if (modulesData && modules.length === 0) {
+    const modulesWithProgress = modulesData.data.docs.map((module) => ({
+      ...module,
+      progress: 50
+    }));
+    dispatch(setModules(modulesWithProgress));
+  }
+
   return (
     <div className="p-4 px-6">
       {/* header */}
       <h1 className="font-inter font-bold text-2xl">Welcome, Choose a module to get started!</h1>
-      {/* Module cards */}
-      <div className=" flex gap-4 mt-8">
-        {modules.map((module) => (
-          <ModuleCard
-            key={module.moduleId}
-            moduleId={module.moduleId}
-            title={module.title}
-            progress={module.progress}
-          />
-        ))}
-      </div>
+
+      {!modulesData ? (
+        <div className="flex justify-center items-center">
+          <Loader /> {/* Show loader while fetching data */}
+        </div>
+      ) : (
+        <Scrollbars
+          autoHide
+          autoHideTimeout={1000}
+          autoHideDuration={200}
+          autoHeight
+          autoHeightMin={0}
+          autoHeightMax={"calc(100vh - 150px)"}
+          thumbMinSize={30}
+          universal={true}
+          className="rounded-lg"
+        >
+          <div className="grid grid-cols-3 gap-4 mt-8 mb-4 mx-1">
+            {modules.length > 0 ? (
+              modules.map((module) => (
+                <ModuleCard key={module._id} moduleId={module._id} title={module.name} progress={module.progress} />
+              ))
+            ) : (
+              <div className="text-center text-lg font-bold">No modules available.</div>
+            )}
+          </div>
+        </Scrollbars>
+      )}
     </div>
   );
 };
