37252f5669bcf8ff87137d979503f65f05558bcf
Danuja Jayasuriya <82136975+danuja01@users.noreply.github.com>
2024-10-19 02:51:11 +0530

    Refactor: Update DueQuizCard component to display ease with two decimal places

--- src/components/cards/due-quiz-card.jsx
+++ src/components/cards/due-quiz-card.jsx
@@ -2,8 +2,8 @@ const DueQuizCard = ({ question, ease, status, attempt }) => {
   return (
     <div className="bg-slate-100 rounded-lg p-4 my-3 shadow-sm shadow-slate-100">
       <p className="text-sm font-inter font-semibold text-left">{question}</p>
-      <div className="text-xs flex justify-between mt-3 text-gray-600 font-inter">
-        <p>Ease: {ease}</p>
+      <div className="text-xs  grid grid-cols-3 mt-3 text-gray-600 font-inter">
+        <p>Ease: {ease.toFixed(2)}</p>
         <p>Attempt: {attempt}</p>
         <p>Status: {status}</p>
       </div>
